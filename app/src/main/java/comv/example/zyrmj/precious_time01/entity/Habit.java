8155a948c70273a263a49c4573516e466b2886c3
walter <tjcxs136720@163.com>
2019-12-09 18:55:04 +0800

    Add data management in habit init. Ref #58

--- app/src/main/java/comv/example/zyrmj/precious_time01/entity/Habit.java
+++ app/src/main/java/comv/example/zyrmj/precious_time01/entity/Habit.java
@@ -4,16 +4,19 @@ import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
+import androidx.room.Ignore;
 import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.Serializable;
+
 @Entity(primaryKeys = {"user_id","habit_name"},
         foreignKeys = @ForeignKey(entity = User.class,
         parentColumns = "id",
         childColumns = "user_id"), indices = @Index(value = "user_id"))
-public class Habit {
+public class Habit implements Serializable {
     @NonNull
     @ColumnInfo(name = "user_id")
     public String userId;
@@ -111,4 +114,6 @@ public class Habit {
         this.length = length;
         this.completion = completion;
     }
+    @Ignore
+    public Habit(){}
 }
