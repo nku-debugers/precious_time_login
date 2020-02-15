343bc870a273f21b1f8740c3a1f0814c0fe81f93
walter <tjcxs136720@163.com>
2020-02-15 16:42:12 +0800

    Notification demo

--- app/src/main/java/comv/example/zyrmj/precious_time01/notification/LongRunningService.java
+++ app/src/main/java/comv/example/zyrmj/precious_time01/notification/LongRunningService.java
@@ -1,13 +1,22 @@
 package comv.example.zyrmj.precious_time01.notification;
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.os.SystemClock;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import comv.example.zyrmj.precious_time01.R;
 
 public class LongRunningService extends Service {
+    private static String TAG = "mytag";
     @Override
     public IBinder onBind(Intent intent) {
         return null;
@@ -15,7 +24,6 @@ public class LongRunningService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
         AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
         //读者可以修改此处的Minutes从而改变提醒间隔时间
         //此处是设置每隔90分钟启动一次
@@ -25,9 +33,11 @@ public class LongRunningService extends Service {
         long triggerAtTime = SystemClock.elapsedRealtime() + Minutes;
         //此处设置开启AlarmReceiver这个Service
         Intent i = new Intent(this, AlarmReceiver.class);
+        i.setAction("notice");
         PendingIntent pi = PendingIntent.getBroadcast(this, 0, i, 0);
         //ELAPSED_REALTIME_WAKEUP表示让定时任务的出发时间从系统开机算起，并且会唤醒CPU。
         manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, pi);
+        Log.d(TAG, "onStartCommand: message sent");
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -38,6 +48,7 @@ public class LongRunningService extends Service {
         //在Service结束后关闭AlarmManager
         AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
         Intent i = new Intent(this, AlarmReceiver.class);
+
         PendingIntent pi = PendingIntent.getBroadcast(this, 0, i, 0);
         manager.cancel(pi);
 
